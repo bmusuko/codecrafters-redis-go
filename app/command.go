bd1e26d2eab9a1e5f377da55f361b7097ca11706
Bram Musuko Panjaitan <bram.musuko@bytedance.com>
2024-07-28 19:58:17 +0800

    Feat: split
    Change-Id: I0f24720cc58eea804213911f460a47cef5f4ab15

--- app/command.go
+++ app/command.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
 	"strconv"
 	"strings"
@@ -15,6 +16,56 @@ type store struct {
 }
 
 var _map sync.Map
+
+func handleCommand(conn net.Conn, strs []string) {
+	command := strings.ToLower(strs[0])
+
+	now := time.Now()
+	var reply string
+	switch command {
+	case "ping":
+		reply = "PONG"
+		conn.Write([]byte(fmt.Sprintf("+%s\r\n", reply)))
+		break
+	case "echo":
+		reply = strs[1]
+		conn.Write([]byte(fmt.Sprintf("+%s\r\n", reply)))
+		break
+	case "set":
+		handleSet(now, strs[1:])
+		if _metaInfo.isMaster() {
+			reply = "OK"
+			conn.Write([]byte(fmt.Sprintf("+%s\r\n", reply)))
+			handleBroadcast(rawBuf)
+		}
+		break
+	case "get":
+		resp, ok := handleGet(now, strs[1])
+		if ok {
+			reply = resp
+			conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(reply), reply)))
+		} else {
+			reply = "-1"
+			conn.Write([]byte(fmt.Sprintf("$%s\r\n", reply)))
+		}
+		break
+	case "info":
+		replies := handleInfo()
+		sendBulkString(conn, replies)
+		break
+	case "replconf":
+		reply = "OK"
+		conn.Write([]byte(fmt.Sprintf("+%s\r\n", reply)))
+		break
+	case "psync":
+		conn.Write([]byte(fmt.Sprintf("+FULLRESYNC %s %d\r\n", _metaInfo.masterReplID, *_metaInfo.masterReplOffset)))
+		time.Sleep(100 * time.Millisecond)
+		fullByte := getEmptyRDBByte()
+		conn.Write([]byte(fmt.Sprintf("$%d\r\n%s", len(fullByte), fullByte)))
+
+		_metaInfo.addSlave(conn)
+	}
+}
 
 func handleSet(now time.Time, strs []string) {
 	key := strs[0]
--- app/conn.go
+++ app/conn.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"net"
 	"strconv"
-	"strings"
-	"time"
 )
 
 func handleClient(conn net.Conn) {
@@ -17,7 +15,6 @@ func handleClient(conn net.Conn) {
 			fmt.Printf("failed to read data\n")
 			return
 		}
-		now := time.Now()
 
 		rawBuf := buf[:n]
 		rawStr := string(rawBuf)
@@ -30,53 +27,6 @@ func handleClient(conn net.Conn) {
 		}
 		fmt.Printf("localhost:%d got %q\n", _metaInfo.port, strs)
 
-		command := strings.ToLower(strs[0])
-
-		var reply string
-		switch command {
-		case "ping":
-			reply = "PONG"
-			conn.Write([]byte(fmt.Sprintf("+%s\r\n", reply)))
-			break
-		case "echo":
-			reply = strs[1]
-			conn.Write([]byte(fmt.Sprintf("+%s\r\n", reply)))
-			break
-		case "set":
-			handleSet(now, strs[1:])
-			if _metaInfo.isMaster() {
-				reply = "OK"
-				conn.Write([]byte(fmt.Sprintf("+%s\r\n", reply)))
-				handleBroadcast(rawBuf)
-			}
-			break
-		case "get":
-			resp, ok := handleGet(now, strs[1])
-			if ok {
-				reply = resp
-				conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(reply), reply)))
-			} else {
-				reply = "-1"
-				conn.Write([]byte(fmt.Sprintf("$%s\r\n", reply)))
-			}
-			break
-		case "info":
-			replies := handleInfo()
-			sendBulkString(conn, replies)
-			break
-		case "replconf":
-			reply = "OK"
-			conn.Write([]byte(fmt.Sprintf("+%s\r\n", reply)))
-			break
-		case "psync":
-			conn.Write([]byte(fmt.Sprintf("+FULLRESYNC %s %d\r\n", _metaInfo.masterReplID, *_metaInfo.masterReplOffset)))
-			time.Sleep(100 * time.Millisecond)
-			fullByte := getEmptyRDBByte()
-			conn.Write([]byte(fmt.Sprintf("$%d\r\n%s", len(fullByte), fullByte)))
-
-			_metaInfo.addSlave(conn)
-		default:
-			continue
-		}
+		handleCommand(conn, strs)
 	}
 }
